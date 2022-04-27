72dc8d843656cd4625e5755ad9bc3b5cb6f6f58f
ErnestCodes <emmycontacts@gmail.com>
2022-04-27 07:09:05 +0100

    Create accessToken and refreshToken

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { createSession } from "../service/session.service";
 import { validatePassword } from "../service/user.service";
+import { signJwt } from "../utils/jwt.utils";
+import config from "config";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
   // Validate d users password
@@ -11,8 +13,30 @@ export async function createUserSessionHandler(req: Request, res: Response) {
   }
   // create a session
   const session = await createSession(user._id, req.get("user-agent") || "");
+
   // create an access token
+  const accessToken = signJwt(
+    {
+      ...user,
+      session: session._id,
+    },
+    {
+      expiresIn: config.get("accessTokenTtl"), // 15 minutes
+    }
+  );
 
   //   create a refresh token
+  const refreshToken = signJwt(
+    {
+      ...user,
+      session: session._id,
+    },
+    {
+      expiresIn: config.get("refreshTokenTtl"), // 15 minutes
+    }
+  );
+
   // return access and refresh token
+
+  return res.send({ accessToken, refreshToken });
 }
