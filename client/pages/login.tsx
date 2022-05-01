77343959e54773348488a850315f23a1cce455d6
ErnestCodes <emmycontacts@gmail.com>
2022-05-01 11:10:57 +0100

    setup login page

---
+++ client/pages/login.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import Login from "../src/components/Login";
+
+const login: NextPage = () => {
+  return <Login />;
+};
+
+export default login;
