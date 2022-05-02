794de923d1f83a4f0a6be74310f868829740b1ae
ErnestCodes <emmycontacts@gmail.com>
2022-05-02 16:29:24 +0100

    fix bug

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
