2378f9549a5e7f23539a895919dd0a95b30781a8
ErnestCodes <emmycontacts@gmail.com>
2022-06-24 15:47:16 +0100

    Create product input

--- client/src/components/Admin/Home/Home.admin.tsx
+++ client/src/components/Admin/Home/Home.admin.tsx
@@ -25,6 +25,7 @@ import {
   SearchIcon,
 } from "@heroicons/react/solid";
 import routes from "../../../routes";
+import { Link } from "react-router-dom";
 
 const navigation = [
   { name: "Home", href: "#", icon: HomeIcon, current: true },
@@ -393,12 +394,14 @@ export default function HomeAdmin() {
                     </div>
                   </div>
                   <div className="mt-6 flex space-x-3 md:mt-0 md:ml-4">
-                    <button
-                      type="button"
-                      className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
-                    >
-                      Add products
-                    </button>
+                    <Link to={routes.new}>
+                      <button
+                        type="button"
+                        className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                      >
+                        Add products
+                      </button>
+                    </Link>
                   </div>
                 </div>
               </div>
