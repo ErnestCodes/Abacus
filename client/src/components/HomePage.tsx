d26081e016688e537f670c617ac55e50dc876380
Izuchukwu Onukwube <57435925+izuchukwu-eric@users.noreply.github.com>
2022-07-11 14:16:55 +0100

    Created footer component

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -1,9 +1,6 @@
 import { Fragment, useEffect, useState } from "react";
 import { Popover, Transition, Dialog, Tab } from "@headlessui/react";
 import {
-  footerNavigation,
-  trendingProducts,
-  collections,
   favorites,
   navigation,
 } from "../utils/data";
@@ -21,6 +18,7 @@ import { loadingUser } from "../features/user/userSlice";
 import { Link } from "react-router-dom";
 import routes from "../routes";
 import HeroSection from "./HeroSection";
+import FooterSection from "./FooterSection";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -936,116 +934,8 @@ function HomePage() {
           </div>
         </section>
       </main>
-
-      <footer aria-labelledby="footer-heading" className="bg-[#f0c14b]">
-        <h2 id="footer-heading" className="sr-only">
-          Footer
-        </h2>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="py-20 xl:grid xl:grid-cols-3 xl:gap-8">
-            <div className="grid grid-cols-2 gap-8 xl:col-span-2">
-              <div className="space-y-16 md:space-y-0 md:grid md:grid-cols-2 md:gap-8">
-                <div>
-                  <h3 className="text-sm font-medium text-white">Shop</h3>
-                  <ul role="list" className="mt-6 space-y-6">
-                    {footerNavigation.shop.map((item) => (
-                      <li key={item.name} className="text-sm">
-                        <a
-                          href={item.href}
-                          className="text-white hover:text-gray-600"
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div>
-                  <h3 className="text-sm font-medium text-white">Company</h3>
-                  <ul role="list" className="mt-6 space-y-6">
-                    {footerNavigation.company.map((item) => (
-                      <li key={item.name} className="text-sm">
-                        <a
-                          href={item.href}
-                          className="text-white hover:text-gray-600"
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="space-y-16 md:space-y-0 md:grid md:grid-cols-2 md:gap-8">
-                <div>
-                  <h3 className="text-sm font-medium text-white">Account</h3>
-                  <ul role="list" className="mt-6 space-y-6">
-                    {footerNavigation.account.map((item) => (
-                      <li key={item.name} className="text-sm">
-                        <a
-                          href={item.href}
-                          className="text-white hover:text-gray-600"
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div>
-                  <h3 className="text-sm font-medium text-white">Connect</h3>
-                  <ul role="list" className="mt-6 space-y-6">
-                    {footerNavigation.connect.map((item) => (
-                      <li key={item.name} className="text-sm">
-                        <a
-                          href={item.href}
-                          className="text-white hover:text-gray-600"
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="mt-16 md:mt-16 xl:mt-0">
-              <h3 className="text-sm font-medium text-white">
-                Sign up for our newsletter
-              </h3>
-              <p className="mt-6 text-sm text-white">
-                The latest deals and savings, sent to your inbox weekly.
-              </p>
-              <form className="mt-2 flex sm:max-w-md">
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  type="text"
-                  autoComplete="email"
-                  required
-                  className="appearance-none min-w-0 w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-4 text-base placeholder-gray-500 focus:outline-none"
-                />
-                <div className="ml-4 flex-shrink-0">
-                  <button
-                    type="submit"
-                    className="w-full bg-gray-800 border border-transparent rounded-md shadow-sm py-2 px-4 flex items-center justify-center text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    Sign up
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-
-          <div className="border-t border-gray-200 py-10">
-            <p className="text-sm text-white">
-              Copyright &copy; 2022 Ares Inc.
-            </p>
-          </div>
-        </div>
-      </footer>
+  
+      <FooterSection />
     </div>
   );
 }
